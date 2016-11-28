efd0c1ca583980335ca23afc37020ba350a50835
lucas <lucas.aragno157@gmail.com>
2016-11-27 21:50:42 -0300

    Remove comment

--- Tutorial/source/PlayState.hx
+++ Tutorial/source/PlayState.hx
@@ -17,20 +17,6 @@ class PlayState extends FlxState
 
 	override public function create():Void
 	{
-		/*
-		_player = new Player(20, 20);
-		_level = new FlxTilemap();
-		var mapData:String  = Assets.getText('assets/data/map.csv');
-		var mapTilePath:String = 'assets/images/tiles.png';
-		_level.loadMap(mapData, mapTilePath, 16, 16);
-		add(_level);
-    //add(_player);
-		super.create();*/
-		level = new TiledLevel("assets/tiled/map.tmx", this);
-		add(level.backgroundLayer);
-		add(level.imagesLayer);
-		add(level.objectsLayer);
-		add(level.foregroundTiles);
 	}
 
 	override public function update(elapsed:Float):Void
